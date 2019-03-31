d35a7060a34ac5eeb93c09e145ac497f393be8c2
Deng XiaoLong <Dragonet_d@163.com>
2019-03-31 22:35:24 +0800

    once function

--- noteAboutVue/数据驱动/index.js
+++ noteAboutVue/数据驱动/index.js
@@ -1,3 +1,5 @@
+import Vue from 'vue'
+
 const sharedPropertyDefinition = {
     enumerable: true,
     configurable: true,
@@ -13,4 +15,26 @@ export function proxy(target, sourceKey, key) {
         this[sourceKey][key] = val
     }
     Object.defineProperty(target, key, sharedPropertyDefinition)
+}
+
+const app = new Vue({
+    el: '#app',
+    mounted() {
+        console.log(this.message)
+        // 通过proxy 代理 实际上方访问的是 this._data.message
+        // proxy(this, '_data', 'message')
+    },
+    data() {
+        message: 'hello vue'
+    }
+})
+
+function once(fn) {
+    let called = false
+    return function () {
+        if (!called) {
+            called = true
+            fn.apply(this, arguments)
+        }
+    }
 }
