19049cf14889a891d958676a582f936c19c23f21
Deng XiaoLong <Dragonet_d@163.com>
2019-04-04 22:52:09 +0800

    merge options

---
+++ note/组件化/mergeOption.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+
+let childComp = {
+  template: '<div>{{msg}}</div>',
+  created() {
+    console.log('child created')
+  },
+  mounted() {
+    console.log('child mounted')
+  },
+  data() {
+    return {
+      msg: 'Hello Vue'
+    }
+  }
+}
+
+Vue.mixin({
+  created() {
+    console.log('parent created')
+  }
+})
+
+let app = new Vue({
+  el: '#app',
+  render: h => h(childComp)
+})
+
+/*
+1,外部调用场景下的合并配置是通过mergeOption,并遵循一定的合并策略
+2,组件的合并是通过initInternalComponent,它的合并更快
+3,框架库的设计都是类似,自身定义了默认配置,同时可以再初始化阶段传入配置,然后merge配置,来达到定制化不同需求的目的;
+*/
--- note/组件化/patch.js
+++ note/组件化/patch.js
@@ -9,7 +9,8 @@ function createComponent(vnode, insertedVnodeQueue, parentElm, refElm) {
 组件vnode有data有hook,
 占位节点 <HelloWorld/> 起占位作用相当于parent vnode
 
-patch 整体流程: createComponent -> 子组件初始化 -> 子组件render -> 子组件patch
+1, patch 整体流程: createComponent -> 子组件初始化 -> 子组件render -> 子组件patch
 
-activeInstance 为当前激活的vm实例; vm.$vnode为组件的占位vnode; vm._vnode为组件的渲染vnode
+2, activeInstance 为当前激活的vm实例; vm.$vnode为组件的占位vnode; vm._vnode为组件的渲染vnode
+3, 嵌套组件的插入顺序是先子后父
 */
