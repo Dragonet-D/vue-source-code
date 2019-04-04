df6ee1c50f0692f825e6230c92613212e9bbba04
Deng XiaoLong <Dragonet_d@163.com>
2019-04-04 22:06:22 +0800

    component patch

--- note/组件化/patch.js
+++ note/组件化/patch.js
@@ -1,6 +1,15 @@
-function createComponent (vnode, insertedVnodeQueue, parentElm, refElm) {
-    let i = vnode.data;
-    if (isDef(i)){
-        const isReactivated = isDef(vnode.compoentInstance) && i.keepAlive;
-    }
+function createComponent(vnode, insertedVnodeQueue, parentElm, refElm) {
+  let i = vnode.data;
+  if (isDef(i)) {
+    const isReactivated = isDef(vnode.compoentInstance) && i.keepAlive;
+  }
 }
+
+/*
+组件vnode有data有hook,
+占位节点 <HelloWorld/> 起占位作用相当于parent vnode
+
+patch 整体流程: createComponent -> 子组件初始化 -> 子组件render -> 子组件patch
+
+activeInstance 为当前激活的vm实例; vm.$vnode为组件的占位vnode; vm._vnode为组件的渲染vnode
+*/
